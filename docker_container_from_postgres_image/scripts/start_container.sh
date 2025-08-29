04add807506898b6217ba075769b2a86b80c6c85
Leonardo Baroncelli <leonardo.baroncelli26@gmail.com>
2025-08-29 13:57:22 +0200

    First commit

---
+++ docker_container_from_postgres_image/scripts/start_container.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PGDATA_ROOT=/var/lib/postgresql/data
+
+docker stop deception-db
+docker container prune
+sudo rm -rf mount
+
+docker run  \
+    --name deception-db -p 5432:5432 -e POSTGRES_PASSWORD=password -e PGDATA=${PGDATA_ROOT}/pg_data \
+    -v "$(pwd)/mount:${PGDATA_ROOT}" \
+    -v "$(pwd)/image_configuration/postgres_configurations/postgres.conf":/home/postgres/postgres_conf/postgres.conf \
+    -v "$(pwd)/image_configuration/postgres_configurations/pg_hba.conf":/home/postgres/postgres_conf/pg_hba.conf \
+    alex176/deception_db:latest \
+    -c "config_file=/home/postgres/postgres_conf/postgres.conf" \
+    -c "hba_file=/home/postgres/postgres_conf/pg_hba.conf"
